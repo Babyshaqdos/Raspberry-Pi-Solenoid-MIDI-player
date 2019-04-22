7685ed516c63bbb137d85613178edd47b11bf4e6
Nick Souligne <44684548+Babyshaqdos@users.noreply.github.com>
2019-04-22 12:37:54 -0500

    Update MIDIReader.py
    Updated build which correctly reads in the midi file, still need to test and make sure the output is going to the correct solenoid and has enough voltage to press the key down on the piano

--- MIDIReader.py
+++ MIDIReader.py
@@ -1,3 +1,5 @@
+import mido
+from mido import MidiFile
 import pygame
 import pygame.midi
 from time import sleep
@@ -5,49 +7,70 @@ from time import sleep
 # Initialize the Pygame and pygame.midi modules
 pygame.init()
 pygame.midi.init()
-startSong = pygame.midi.midis2events("pianomidi.mid", -1)
 
 
-def reader(startSong):
-  while startSong.hasNext:
-    if startSong.byte1 == 0x9:
-        handleNotes(
-        startSong.byte1,
-        startSong.byte2,
-        startSong.byte3
-        );
-        print(handleNotes())
-        break
-    break
 
 
-def handleNotes(channel, pitch, velocity):
-  port = 0;
-  latency = 1;
+mid = MidiFile('pianomidi.mid')
+
+def reader():
+    for i, track in enumerate(mid.tracks):
+        print('Track {}: {}'.format(i, track.name))
+        for msg in track:
+            if msg.is_meta:
+                pass
+            else:
+                if msg.type == 'note_on':
+                    m = msg.bytes()
+                    for bytes in m:
+                        print (bytes)       # Printing just to confirm message is being read
+                        handleNotes(bytes, bytes, bytes)
+                print(msg)      # Again printing to check its reading correctly
+
+
+
+
+def handleNotes(channel, note, velocity):
+  port = 0
   instrument = 0
-  velocity = 127
-  midiOutput = pygame.midi.Output(port, latency)
+  midiOutput = pygame.midi.Output(port, velocity)
   midiOutput.set_instrument(instrument)
-  if pitch == 24:
-    midiOutput = pygame.midi.Output(2, 1)
-  if pitch == 25:
-    midiOutput = pygame.midi.Output(3, 1)
-  if pitch == 26:
-    midiOutput = pygame.midi.Output(7, 1)
-  if pitch == 27:
-    midiOutput = pygame.midi.Output(8, 1)
-  if pitch == 28:
-    midiOutput = pygame.midi.Output(10, 1)
-  if pitch == 29:
-    midiOutput = pygame.midi.Output(11, 1)
-  if pitch == 30:
-    midiOutput = pygame.midi.Output(12, 1)
-
-  for note in range(24, 30):
-    midiOutput.note_on(note, velocity)
+  if note >= 0 & note < 39: # Change the note range to 1 value
+    port = 2
+    midiOutput.note_on(port, velocity)
     sleep(.25)
-    midiOutput.note_off(note, velocity)
+    midiOutput.note_off(port, velocity)
+  if note >= 39 & note < 54:
+    port = 3
+    midiOutput.note_on(port, velocity)
+    sleep(.25)
+    midiOutput.note_off(port, velocity)
+  if note >= 54 & note < 69:
+    port = 7
+    midiOutput.note_on(port, velocity)
+    sleep(.25)
+    midiOutput.note_off(port, velocity)
+  if note >= 69 & note < 84:
+    port = 8
+    midiOutput.note_on(port, velocity)
+    sleep(.25)
+    midiOutput.note_off(port, velocity)
+  if note >= 84 & note < 99:
+    port = 10
+    midiOutput.note_on(port, velocity)
+    sleep(.25)
+    midiOutput.note_off(port, velocity)
+  if note >= 99 & note < 114:
+    port = 11
+    midiOutput.note_on(port, velocity)
+    sleep(.25)
+    midiOutput.note_off(port, velocity)
+  if note >= 114:
+    port = 12
+    midiOutput.note_on(port, velocity)
+    sleep(.25)
+    midiOutput.note_off(port, velocity)
 
 
-print(reader(startSong))
-pygame.midi.quit()
+
+reader()
